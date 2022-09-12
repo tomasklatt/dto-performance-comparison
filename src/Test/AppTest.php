beacc2620cb269501d035a69de56e66c5195b728
Tomas Klatt <tomas.klatt@livesport.eu>
2022-09-12 09:03:21 +0200

    added DataGeneratorTest

---
+++ src/Test/AppTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DTOPerformanceComparison\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+
+}
---
+++ src/Test/DataGeneratorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DTOPerformanceComparison\Test;
+
+use DTOPerformanceComparison\DataGenerator;
+use PHPUnit\Framework\TestCase;
+
+class DataGeneratorTest extends TestCase
+{
+    const LEAGUES_COUNT_EMPTY = 0;
+    const LEAGUES_COUNT = 15;
+
+    public function testGenerateEmpty(): void
+    {
+        $instance = new DataGenerator(self::LEAGUES_COUNT_EMPTY);
+        $data = $instance->generate();
+        $this->assertIsArray($data);
+        $this->assertEmpty($data);
+        $this->assertEquals(self::LEAGUES_COUNT_EMPTY, $instance->getTopLevelObjectCount());
+        $this->assertEquals(self::LEAGUES_COUNT_EMPTY, $instance->getTotalObjectCount());
+    }
+
+    public function testGenerate(): void
+    {
+        $instance = new DataGenerator(self::LEAGUES_COUNT);
+        $data = $instance->generate();
+        $this->assertIsArray($data);
+        $this->assertIsArray($data['leagues']);
+        $this->assertCount(self::LEAGUES_COUNT, $data['leagues']);
+        $this->assertEquals(self::LEAGUES_COUNT, $instance->getTopLevelObjectCount());
+        $this->assertTrue(self::LEAGUES_COUNT < $instance->getTotalObjectCount());
+    }
+
+}
